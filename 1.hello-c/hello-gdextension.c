c6f65bda30760199f1b6921796bcfa1bde7c60bb
gilzoide <gilzoide@gmail.com>
2024-01-04 09:13:13 -0300

    Update hello-gdextension.c with Godot 4.2 support

--- 1.hello-c/hello-gdextension.c
+++ 1.hello-c/hello-gdextension.c
@@ -12,39 +12,25 @@ typedef struct {
     uint8_t godot_data_dont_touch_this[24];
 } Variant;
 
-// GDExtensions interface pointer
-const GDExtensionInterface *interface;
+// GDExtension API function pointers
+static GDExtensionInterfaceStringNewWithUtf8Chars string_new_with_utf8_chars;
+static GDExtensionInterfaceStringNameNewWithUtf8Chars string_name_new_with_utf8_chars;
+static GDExtensionInterfaceGetVariantFromTypeConstructor get_variant_from_type_constructor;
+static GDExtensionInterfaceVariantGetPtrDestructor variant_get_ptr_destructor;
+static GDExtensionInterfaceVariantGetPtrUtilityFunction variant_get_ptr_utility_function;
+static GDExtensionInterfaceVariantDestroy variant_destroy;
 // Godot API function pointers
-static GDExtensionPtrConstructor construct_StringName_from_String;
 static GDExtensionVariantFromTypeConstructorFunc construct_Variant_from_String;
 static GDExtensionPtrDestructor destroy_String;
 static GDExtensionPtrDestructor destroy_StringName;
 
-// here is our global "print" StringName
-static StringName print_StringName;
-// and here is our global "print" function
+// Here is our global "print" function
 static GDExtensionPtrUtilityFunction print_function;
-
-StringName construct_StringName_from_cstring(const char *text) {
-    // 1. Construct a String from the C string
-    String string;
-    interface->string_new_with_latin1_chars(&string, text);
-
-    // 2. Construct a StringName from the String
-    StringName string_name;
-    GDExtensionConstTypePtr constructor_arguments[1] = { &string };
-    construct_StringName_from_String(&string_name, constructor_arguments);
-
-    // 3. Destroy the String, since it's not needed anymore
-    destroy_String(&string);
-
-    return string_name;
-}
 
 void print(const char *text) {
     // 1. Construct a String from the C string
     String string;
-    interface->string_new_with_utf8_chars(&string, text);
+    string_new_with_utf8_chars(&string, text);
 
     // 2. Construct a Variant from the String
     Variant arg1;
@@ -56,7 +42,7 @@ void print(const char *text) {
     print_function(NULL, args, 1);
 
     // 4. Clean up resources that are no longer needed
-    interface->variant_destroy(&arg1);
+    variant_destroy(&arg1);
     destroy_String(&string);
 }
 
@@ -65,17 +51,17 @@ void initialize(void *userdata, GDExtensionInitializationLevel p_level) {
         return;
     }
 
-    // StringName constructor at index 2 is the one that receives String
-    // You can find this information in `extension_api.json` file
-    construct_StringName_from_String = interface->variant_get_ptr_constructor(GDEXTENSION_VARIANT_TYPE_STRING_NAME, 2);
-    construct_Variant_from_String = interface->get_variant_from_type_constructor(GDEXTENSION_VARIANT_TYPE_STRING);
-    destroy_String = interface->variant_get_ptr_destructor(GDEXTENSION_VARIANT_TYPE_STRING);
-    destroy_StringName = interface->variant_get_ptr_destructor(GDEXTENSION_VARIANT_TYPE_STRING_NAME);
+    construct_Variant_from_String = get_variant_from_type_constructor(GDEXTENSION_VARIANT_TYPE_STRING);
+    destroy_String = variant_get_ptr_destructor(GDEXTENSION_VARIANT_TYPE_STRING);
+    destroy_StringName = variant_get_ptr_destructor(GDEXTENSION_VARIANT_TYPE_STRING_NAME);
 
     // Initialize "print" StringName
-    print_StringName = construct_StringName_from_cstring("print");
+    StringName print_StringName;
+    string_name_new_with_utf8_chars(&print_StringName, "print");
     // then fetch the "print" function pointer
-    print_function = interface->variant_get_ptr_utility_function(&print_StringName, 2648703342);
+    print_function = variant_get_ptr_utility_function(&print_StringName, 2648703342);
+    // Release the "print" StringName, since we don't need it anymore
+    destroy_StringName(&print_StringName);
 
     print("Hello GDExtension from C!");
 }
@@ -86,19 +72,21 @@ void deinitialize(void *userdata, GDExtensionInitializationLevel p_level) {
     }
 
     print("Goodbye GDExtension from C!");
-
-    // Destroy "print" StringName
-    destroy_StringName(&print_StringName);
 }
 
 GDExtensionBool hello_extension_entry(
-    const GDExtensionInterface *p_interface,
+    GDExtensionInterfaceGetProcAddress p_get_proc_address,
     GDExtensionClassLibraryPtr p_library,
     GDExtensionInitialization *r_initialization
 ) {
     r_initialization->initialize = &initialize;
     r_initialization->deinitialize = &deinitialize;
-    // save the GDExtensionInterface globally
-    interface = p_interface;
+    // save the GDExtension API function pointers globally
+    string_new_with_utf8_chars = (GDExtensionInterfaceStringNewWithUtf8Chars) p_get_proc_address("string_new_with_utf8_chars");
+    string_name_new_with_utf8_chars = (GDExtensionInterfaceStringNameNewWithUtf8Chars) p_get_proc_address("string_name_new_with_utf8_chars");
+    get_variant_from_type_constructor = (GDExtensionInterfaceGetVariantFromTypeConstructor) p_get_proc_address("get_variant_from_type_constructor");
+    variant_get_ptr_destructor = (GDExtensionInterfaceVariantGetPtrDestructor) p_get_proc_address("variant_get_ptr_destructor");
+    variant_get_ptr_utility_function = (GDExtensionInterfaceVariantGetPtrUtilityFunction) p_get_proc_address("variant_get_ptr_utility_function");
+    variant_destroy = (GDExtensionInterfaceVariantDestroy) p_get_proc_address("variant_destroy");
     return 1;
 }
